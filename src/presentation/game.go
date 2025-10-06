6b6f220d31e9a4e66d3c31ff2a272fdca4a14c56
Ivan Kalashnikov <ivank.public@gmail.com>
2025-10-07 00:10:43 +0700

    Реализация базовой структуры игры и интерфейса для игровых состояний.

--- src/presentation/game.go
+++ src/presentation/game.go
@@ -1,1 +1,51 @@
 package presentation
+
+type GameStateSignal int
+
+const (
+	StopStateSignal GameStateSignal = iota
+)
+
+type GameState interface {
+	Input()
+	Render()
+	Update() GameStateSignal
+}
+
+type Game struct {
+	States []GameState
+}
+
+func (g *Game) PushState(state GameState) {
+	g.States = append(g.States, state)
+}
+
+func (g *Game) PopState() {
+	if len(g.States) == 0 {
+		return
+	}
+	g.States = g.States[:len(g.States)-1]
+}
+
+func (g *Game) CurrentState() GameState {
+	if len(g.States) == 0 {
+		return nil
+	}
+	return g.States[len(g.States)-1]
+}
+
+func (g *Game) Run() {
+	for {
+		state := g.CurrentState()
+		if state == nil {
+			break
+		}
+		state.Input()
+		sign := state.Update()
+		state.Render()
+
+		if sign == StopStateSignal {
+			g.PopState()
+		}
+	}
+}
