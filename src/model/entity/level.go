5adb16dc45197bb34bcffe82c9afc9f645c517dd
Pilyaeva Polina <pilyaevapolina@gmail.com>
2025-10-27 14:14:55 +0300

    Добавлены дополнительные тоннели. Сокращена логика генерации тоннелей.

--- src/model/entity/level.go
+++ src/model/entity/level.go
@@ -2,6 +2,7 @@ package entity
 
 import (
 	"errors"
+	"math"
 	"math/rand"
 )
 
@@ -31,35 +32,22 @@ var gridNeighborsRooms = [][]int{
 	{5, 7},
 }
 
-var xPassages = [][2]int{
-	{0, 1},
-	{1, 2},
-	{3, 4},
-	{4, 5},
-	{6, 7},
-	{7, 8},
+var horizontalNeighborRoomsSet = map[[2]int]struct{}{
+	{0, 1}: {},
+	{1, 2}: {},
+	{3, 4}: {},
+	{4, 5}: {},
+	{6, 7}: {},
+	{7, 8}: {},
 }
 
-var yPassages = [][2]int{
-	{0, 3},
-	{3, 6},
-	{1, 4},
-	{4, 7},
-	{2, 5},
-	{5, 8},
-}
-
-func calculateRoomSectionSize(mapSize Size2D[uint]) (sectionSize Size2D[uint]) {
-	totalPaddingWidth := uint(MinRoomPadding * 2)
-	totalPaddingHeight := uint(MinRoomPadding * 2)
-
-	availableWidth := mapSize.Width - totalPaddingWidth
-	availableHeight := mapSize.Height - totalPaddingHeight
-
-	sectionSize.Width = availableWidth / numberXYSections
-	sectionSize.Height = availableHeight / numberXYSections
-
-	return sectionSize
+var verticalNeighborRoomsSet = map[[2]int]struct{}{
+	{0, 3}: {},
+	{3, 6}: {},
+	{1, 4}: {},
+	{4, 7}: {},
+	{2, 5}: {},
+	{5, 8}: {},
 }
 
 func (l *Level) GenerateNineRooms(sizeMap Size2D[uint]) error {
@@ -128,6 +116,133 @@ func (l *Level) GenerateNineRooms(sizeMap Size2D[uint]) error {
 	return nil
 }
 
+func calculateRoomSectionSize(mapSize Size2D[uint]) (sectionSize Size2D[uint]) {
+	totalPaddingWidth := uint(MinRoomPadding * 2)
+	totalPaddingHeight := uint(MinRoomPadding * 2)
+
+	availableWidth := mapSize.Width - totalPaddingWidth
+	availableHeight := mapSize.Height - totalPaddingHeight
+
+	sectionSize.Width = availableWidth / numberXYSections
+	sectionSize.Height = availableHeight / numberXYSections
+
+	return sectionSize
+}
+
+func (l *Level) GeneratePassages() error {
+	roomIndex := rand.Intn(9)
+	treeEdges, err := generateSpanningTree(roomIndex)
+	if err != nil {
+		return err
+	}
+
+	extraEdgesCount := rand.Intn(4)
+	treeEdges = addRandomEdges(treeEdges, extraEdgesCount)
+
+	for _, connectedRooms := range treeEdges {
+		minIndex, maxIndex := sortByOrderAsc(connectedRooms[0], connectedRooms[1])
+		key := [2]int{minIndex, maxIndex}
+		if _, exists := horizontalNeighborRoomsSet[key]; exists {
+			doorOne := getDoorRightWall(l.Rooms[minIndex])
+			doorTwo := getDoorLeftWall(l.Rooms[maxIndex])
+			l.Passages = append(l.Passages, *NewPassageOnX(doorOne, doorTwo))
+		}
+		if _, exists := verticalNeighborRoomsSet[key]; exists {
+			doorOne := getDoorDownWall(l.Rooms[minIndex])
+			doorTwo := getDoorTopWall(l.Rooms[maxIndex])
+			l.Passages = append(l.Passages, *NewPassageOnY(doorOne, doorTwo))
+		}
+	}
+
+	return nil
+}
+
+func generateSpanningTree(startRoom int) ([][2]int, error) {
+	if startRoom < 0 || startRoom > 9 {
+		return nil, errors.New("start room must be between 0 and 9")
+	}
+
+	edges := make([][2]int, 0)
+	stack := []int{startRoom}
+	visit := make(map[int]bool)
+	visit[startRoom] = true
+
+	for len(stack) > 0 {
+		currentRoom := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		allNeighborsRooms := gridNeighborsRooms[currentRoom]
+		unvisitNeighborsRooms := []int{}
+		for _, neighborRoom := range allNeighborsRooms {
+			if !visit[neighborRoom] {
+				unvisitNeighborsRooms = append(unvisitNeighborsRooms, neighborRoom)
+			}
+		}
+
+		// rand.Shuffle перемешивает значения в существующем слайсе unvisitNeighborsRooms
+		rand.Shuffle(len(unvisitNeighborsRooms), func(i, j int) {
+			unvisitNeighborsRooms[i], unvisitNeighborsRooms[j] = unvisitNeighborsRooms[j], unvisitNeighborsRooms[i]
+		})
+
+		for _, nextRoom := range unvisitNeighborsRooms {
+			edges = append(edges, [2]int{currentRoom, nextRoom})
+			visit[nextRoom] = true
+
+			stack = append(stack, nextRoom)
+		}
+	}
+
+	return edges, nil
+}
+
+func addRandomEdges(sourceEdges [][2]int, extraEdgesCount int) [][2]int {
+	existingConnections := make(map[[2]int]struct{})
+	for _, connectedRooms := range sourceEdges {
+		minIndex, maxIndex := sortByOrderAsc(connectedRooms[0], connectedRooms[1])
+		key := [2]int{minIndex, maxIndex}
+		existingConnections[key] = struct{}{}
+	}
+
+	unionSet := make(map[[2]int]struct{})
+
+	for k := range horizontalNeighborRoomsSet {
+		unionSet[k] = struct{}{}
+	}
+
+	for k := range verticalNeighborRoomsSet {
+		unionSet[k] = struct{}{}
+	}
+
+	resultSet := make(map[[2]int]struct{})
+
+	for k := range unionSet {
+		if _, existsInAnother := existingConnections[k]; !existsInAnother {
+			resultSet[k] = struct{}{}
+		}
+	}
+
+	potentialEdges := make([][2]int, len(resultSet))
+	for k := range resultSet {
+		potentialEdges = append(potentialEdges, k)
+	}
+
+	rand.Shuffle(len(potentialEdges), func(i, j int) {
+		potentialEdges[i], potentialEdges[j] = potentialEdges[j], potentialEdges[i]
+	})
+
+	resultEdges := make([][2]int, len(sourceEdges))
+	for i := range sourceEdges {
+		resultEdges[i] = sourceEdges[i]
+	}
+
+	for i := 0; i < extraEdgesCount; i++ {
+		element := potentialEdges[i]
+		resultEdges = append(resultEdges, [2]int{element[0], element[1]})
+	}
+
+	return resultEdges
+}
+
 func getDoorLeftWall(room Room) Point2D[int] {
 	const wall = 1
 	doorYFrom := room.Shape.Point.Y + wall
@@ -156,83 +271,8 @@ func getDoorDownWall(room Room) Point2D[int] {
 	return Point2D[int]{X: rand.Intn(doorXTo-doorXFrom) + doorXFrom, Y: room.Shape.Point.Y + int(room.Shape.Size.Height)}
 }
 
-func generatePassagesTree(startRoom int) (map[int][]int, error) {
-	if startRoom < 0 || startRoom > 9 {
-		return nil, errors.New("start room must be between 0 and 9")
-	}
-	graph := make(map[int][]int)
-	stack := []int{startRoom}
-	visit := make(map[int]bool)
-	visit[startRoom] = true
-
-	for len(stack) > 0 {
-		currentRoom := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-
-		allNeighborsRooms := gridNeighborsRooms[currentRoom]
-		unvisitNeighborsRooms := []int{}
-		for _, neighborRoom := range allNeighborsRooms {
-			if !visit[neighborRoom] {
-				unvisitNeighborsRooms = append(unvisitNeighborsRooms, neighborRoom)
-			}
-		}
-
-		// rand.Shuffle перемешивает значения в слайсе unvisitNeighborsRooms
-		rand.Shuffle(len(unvisitNeighborsRooms), func(i, j int) {
-			unvisitNeighborsRooms[i], unvisitNeighborsRooms[j] = unvisitNeighborsRooms[j], unvisitNeighborsRooms[i]
-		})
-
-		for _, nextRoom := range unvisitNeighborsRooms {
-			graph[currentRoom] = append(graph[currentRoom], nextRoom)
-			graph[nextRoom] = append(graph[nextRoom], currentRoom)
-			visit[nextRoom] = true
-
-			stack = append(stack, nextRoom)
-		}
-	}
-
-	return graph, nil
-}
-
-func (l *Level) GeneratePassages() error {
-	tree, err := generatePassagesTree(0)
-	if err != nil {
-		return err
-	}
-
-	// [2]int - две комнаты, между которыми тоннель. bool - наличие тоннеля.
-	laidPassages := make(map[[2]int]bool)
-	for roomOne, connectedRooms := range tree {
-		for _, roomTwo := range connectedRooms {
-			var key [2]int
-			if roomOne < roomTwo {
-				key = [2]int{roomOne, roomTwo}
-			} else {
-				key = [2]int{roomTwo, roomOne}
-			}
-			if !laidPassages[key] {
-				laidPassages[key] = true
-				for _, passageKey := range xPassages {
-					if key == passageKey {
-						indexOne := passageKey[0]
-						indexTwo := passageKey[1]
-						doorOne := getDoorRightWall(l.Rooms[indexOne])
-						doorTwo := getDoorLeftWall(l.Rooms[indexTwo])
-						l.Passages = append(l.Passages, *NewPassageOnX(doorOne, doorTwo))
-					}
-				}
-				for _, passageKey := range yPassages {
-					if key == passageKey {
-						indexOne := passageKey[0]
-						indexTwo := passageKey[1]
-						doorOne := getDoorDownWall(l.Rooms[indexOne])
-						doorTwo := getDoorTopWall(l.Rooms[indexTwo])
-						l.Passages = append(l.Passages, *NewPassageOnY(doorOne, doorTwo))
-					}
-				}
-			}
-		}
-	}
-
-	return nil
+func sortByOrderAsc(first, second int) (int, int) {
+	minIndex := int(math.Min(float64(first), float64(second)))
+	maxIndex := int(math.Max(float64(first), float64(second)))
+	return minIndex, maxIndex
 }
