8ef00bd83795d4e1db12554072bc95315ef0afba
Ivan Kalashnikov <ivank.public@gmail.com>
2025-11-11 23:14:17 +0700

    Временное отключение некоторых функций Scroll.

--- src/internal/model/items/scroll.go
+++ src/internal/model/items/scroll.go
@@ -7,39 +7,39 @@ type Scroll struct {
 	AffectedAttribute primitives.Attributes
 }
 
-func NewScroll(box primitives.Box) *Scroll {
-	scrollNames := []string{
-		"Scroll of Shadowstep",
-		"Parchment of Eternal Flame",
-		"Manuscript of Forgotten Truths",
-		"Scroll of Iron Will",
-		"Vellum of the Void",
-		"Scroll of Whispers",
-		"Tome of the Lost King",
-		"Scroll of Unseen Paths",
-		"Parchment of Thunderous Roar",
-	}
+// func NewScroll(box primitives.Box) *Scroll {
+// 	scrollNames := []string{
+// 		"Scroll of Shadowstep",
+// 		"Parchment of Eternal Flame",
+// 		"Manuscript of Forgotten Truths",
+// 		"Scroll of Iron Will",
+// 		"Vellum of the Void",
+// 		"Scroll of Whispers",
+// 		"Tome of the Lost King",
+// 		"Scroll of Unseen Paths",
+// 		"Parchment of Thunderous Roar",
+// 	}
 
-	return &Scroll{
-		Item: Item{
-			Shape: box,
-			Name:  getAttributeRandomName(scrollNames),
-		},
-		AffectedAttribute: getRandomAttribute(),
-	}
-}
+// 	return &Scroll{
+// 		Item: Item{
+// 			Shape: box,
+// 			Name:  getAttributeRandomName(scrollNames),
+// 		},
+// 		AffectedAttribute: getRandomAttribute(),
+// 	}
+// }
 
-func (s *Scroll) Take() {
-	s.Item.Take()
-}
+// func (s *Scroll) Take() {
+// 	s.Item.Take()
+// }
 
-func (s *Scroll) Drop(box primitives.Box) {
-	s.Item.Drop(box)
-}
+// func (s *Scroll) Drop(box primitives.Box) {
+// 	s.Item.Drop(box)
+// }
 
-func (s *Scroll) Use() primitives.Attributes {
-	return s.AffectedAttribute
-}
+// func (s *Scroll) Use() primitives.Attributes {
+// 	return s.AffectedAttribute
+// }
 
 func AsScroll(item any) *Scroll {
 	s, ok := item.(*Scroll)
