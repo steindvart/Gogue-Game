0f01fe9c61f7fe28fd10c721a877662bb05ad12d
Ivan Kalashnikov <ivank.public@gmail.com>
2025-11-11 14:34:19 +0700

    Добавление пакета utils и сущностей в него для работы со случайными числами.

---
+++ src/internal/utils/random.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"math"
+	"math/rand"
+)
+
+type RandomSource interface {
+	Intn(n int) int
+	Float64() float64
+	Perm(n int) []int
+	Shuffle(n int, swap func(i, j int))
+	Uint32() uint32
+	Seed(seed int64)
+}
+
+type RandomGenerator struct {
+	rng *rand.Rand
+}
+
+func NewRandomGeneratorWithSeed(seed int64) *RandomGenerator {
+	return &RandomGenerator{
+		rng: rand.New(rand.NewSource(seed)),
+	}
+}
+
+func (r *RandomGenerator) Intn(n int) int {
+	return r.rng.Intn(n)
+}
+
+func (r *RandomGenerator) Float64() float64 {
+	return r.rng.Float64()
+}
+
+func (r *RandomGenerator) Perm(n int) []int {
+	return r.rng.Perm(n)
+}
+
+func (r *RandomGenerator) Shuffle(n int, swap func(i, j int)) {
+	r.rng.Shuffle(n, swap)
+}
+
+func (r *RandomGenerator) Uint32() uint32 {
+	return r.rng.Uint32()
+}
+
+func (r *RandomGenerator) Seed(seed int64) {
+	r.rng.Seed(seed)
+}
+
+func RandomFloatInRange(random RandomSource, min, max float64) float64 {
+	return min + random.Float64()*(max-min)
+}
+
+func RandomRoundedFloatInRange(random RandomSource, min, max float64) int {
+	return int(math.Round(RandomFloatInRange(random, min, max)))
+}
+
+func RandomIntInRange(random RandomSource, min, max int) int {
+	return min + random.Intn(max-min+1)
+}
