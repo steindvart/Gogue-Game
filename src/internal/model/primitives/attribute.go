40fcf65e21d6d3794b90e3f9d2a75ee50119f59b
Ivan Kalashnikov <ivank.public@gmail.com>
2025-11-19 15:52:31 +0700

    Удаление неиспользуемого метода.

--- src/internal/model/primitives/attribute.go
+++ src/internal/model/primitives/attribute.go
@@ -11,18 +11,6 @@ type Attributes struct {
 	MaxHealth, Health, Agility, Strength float64
 }
 
-func (a *Attributes) GetAffectedAttributeName() string {
-	if a.MaxHealth == 1 && a.Agility == 0 && a.Strength == 0 {
-		return "MaxHealth"
-	} else if a.Agility == 1 && a.MaxHealth == 0 && a.Strength == 0 {
-		return "Agility"
-	} else if a.Strength == 1 && a.MaxHealth == 0 && a.Agility == 0 {
-		return "Strength"
-	} else {
-		return "None"
-	}
-}
-
 func (a *Attributes) Affect(delta *Attributes) {
 	a.MaxHealth += delta.MaxHealth
 	a.Health += delta.Health
